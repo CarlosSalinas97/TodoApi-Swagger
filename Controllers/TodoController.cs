787ffb5cba773c716a0383ccc6839b587acd7991
Carlos Salinas <carlos.ariel.salinas@hotmail.com>
2020-05-15 13:10:47 -0300

    Metodos GET y PUT documentados

---
+++ Controllers/TodoController.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TodoApi.Models;
+
+namespace TodoApi.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/todo")]
+    [ApiController]
+    public class TodoController : Controller
+    {
+        public TodoController(ITodoRepository todoItems)
+        {
+            TodoItems = todoItems;
+        }
+        public ITodoRepository TodoItems { get; set; }
+
+        [HttpGet]
+        public IEnumerable<TodoItem> GetAll()
+        {
+            return TodoItems.GetAll();
+        }
+
+        [HttpGet("{id}", Name = "GetTodo")]
+        public IActionResult GetById(string id)
+        {
+            var item = TodoItems.Find(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            return new ObjectResult(item);
+        }
+
+
+        /// <summary>
+        /// Crea un item ToDo.
+        /// </summary>
+        /// <remarks>
+        /// Ejemplo:
+        ///
+        ///     POST /Todo
+        ///     {
+        ///        "key": "Identificador del item", (Si se omite, se agrega una key por defecto)
+        ///        "name": "Nombre del item",
+        ///        "isComplete": true
+        ///     }
+        ///
+        /// </remarks>
+        /// /// <param name="item"></param>
+        /// <returns>Se ha creado un nuevo item ToDo.</returns>
+        /// <response code="201">El elemento se ha creado correctamente.</response>
+        /// <response code="400">Si el elemento es nulo. El elemento no se creó.</response>   
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public IActionResult Create([FromBody] TodoItem item)
+        {
+            if (item == null)
+            {
+                return BadRequest();
+            }
+            TodoItems.Add(item);
+            return CreatedAtRoute("GetTodo", new { id = item.Key }, item);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(string id, [FromBody] TodoItem item)
+        {
+            if (item == null || item.Key != id)
+            {
+                return BadRequest();
+            }
+
+            var todo = TodoItems.Find(id);
+            if (todo == null)
+            {
+                return NotFound();
+            }
+
+            TodoItems.Update(item);
+            return new NoContentResult();
+        }
+
+        /// <summary>
+        /// Borra un item especifico de ToDo.
+        /// </summary>
+        /// <param name="id"></param>    
+        [HttpDelete("{id}")]
+        public IActionResult Delete(string id)
+        {
+            var todo = TodoItems.Find(id);
+            if (todo == null)
+            {
+                return NotFound();
+            }
+
+            TodoItems.Remove(id);
+            return new NoContentResult();
+        }
+    }
+}
